62baf10c6aa022a6aaef8baf3e7a68e2a3e085d8
Andy Le <andye1201@gmail.com>
2018-11-09 14:39:22 -0500

    Method to check if game over
    reduce redundant code

--- src/com/prog32758/AnswerSelect.java
+++ src/com/prog32758/AnswerSelect.java
@@ -9,17 +9,17 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import javax.xml.ws.Response;
 
 @WebServlet("/AnswerSelect")
 public class AnswerSelect extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
+	
 	public AnswerSelect() {
 		super();
 	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		RequestDispatcher rd = request.getRequestDispatcher("board.jsp");
 		HttpSession session = request.getSession();
 		Player p = (Player) session.getAttribute("Player");
@@ -29,38 +29,34 @@ public class AnswerSelect extends HttpServlet {
 
 		String button = request.getParameter("continue"); // determine skip or answer btn
 
-		if (button.equals("Skip Question")) { // if skip question
-
-			for (Question question : lq.getQuestions()) { // check if game over
-				if (question.getDisabled().equals("disabled")) {
-					isFinished = true;
-					rd = request.getRequestDispatcher("gameover.jsp");
-				} else {
-					rd = request.getRequestDispatcher("board.jsp");
-					break;
-				}
-			}
-		} else if (button.equals("Final Answer")) { // if answer question
+		if (button.equals("Skip Question")) { // if skip question...
+			rd = request.getRequestDispatcher(checkGameOver(lq)); 
+			q.setValue(q.getOldValue());	//for board.jsp generator functionality
+		} else if (button.equals("Final Answer")) { // if answer question...
 			String answer = request.getParameter("answer");
-			if (answer.equals(q.getaCorrect())) {
+			if (answer.equals(q.getaCorrect())) {		// ... check if correct
 				p.setScore(p.getScore() + q.getValue());
 			} else {
 				p.setScore(p.getScore() - q.getValue());
 			}
 
-			for (Question question : lq.getQuestions()) { // check if game over
-				if (question.getDisabled().equals("disabled")) {
-					isFinished = true;
-					rd = request.getRequestDispatcher("gameover.jsp");
-				} else {
-					rd = request.getRequestDispatcher("board.jsp");
-					break;
-				}
-			}
-			q.setValue(q.getOldValue());	//for board.jsp generator
+			rd = request.getRequestDispatcher(checkGameOver(lq)); 
+			q.setValue(q.getOldValue());	//for board.jsp generator functionality			
 		}
 		rd.forward(request, response);
-
+		
+	}
+	
+	protected String checkGameOver(LoadQuestions lq) {
+		String rd = "";
+		for (Question q: lq.getQuestions()) {
+			if (q.getDisabled().equals("disabled")) {
+				rd = "gameover.jsp";
+			} else {
+				rd = "board.jsp";
+			}
+		}
+		return rd;
 	}
 
 }
